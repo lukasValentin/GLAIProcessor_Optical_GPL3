be07ff4166db82defa46dc3d04ed74c5a326bf8b
lukas <graflu@ethz.ch>
2023-10-28 10:08:50 +0200

    adding setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glai_processor',
+    version='0.1',
+    description='A package to invert satellite data to retrieve the green leaf area index (GLAI) and other vegetation traits.',
+    author='Lukas Valentin Graf',
+    author_email='lukas.graf@terensis.io',
+    packages=find_packages(),
+    install_requires=[
+        'rio-cog',
+        'eodal',
+        'git+https://github.com/EOA-team/rtm_inv',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
